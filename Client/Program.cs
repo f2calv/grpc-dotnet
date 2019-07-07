4cea9ffe4fef1b13b405eefea938046f0c894f76
Alex Vincent <alex@far2cool.com>
2019-07-07 09:22:55 +0200

    basic additions/refactoring

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,15 +9,24 @@ namespace CasCap
     {
         static async Task Main(string[] args)
         {
+            await Run();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        async Task Run()
+        {
             AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
             var httpClient = new HttpClient();
             // The port number(50051) must match the port of the gRPC server.
             httpClient.BaseAddress = new Uri("http://localhost:50051");
             var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
-            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
-            Console.WriteLine("Greeting: " + reply.Message);
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            for (var i = 0; i < 10; i++)
+            {
+                var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+                Console.WriteLine("Greeting: " + reply.Message);
+                await Task.Delay(1_000);
+            }
         }
     }
 }
